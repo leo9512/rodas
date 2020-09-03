6604d0ed97bd06f8c6ea68a315927f24cd235fc7
mamarend <mamarend@bancolombia.com.co>
2020-09-03 10:36:34 -0500

    Se realizan cambios a archivo ManejadorGenerarGarantia

--- src/main/java/com/ceiba/tiendatecnologica/aplicacion/manejadores/garantia/ManejadorGenerarGarantia.java
+++ src/main/java/com/ceiba/tiendatecnologica/aplicacion/manejadores/garantia/ManejadorGenerarGarantia.java
@@ -5,29 +5,35 @@ import com.ceiba.tiendatecnologica.dominio.GarantiaExtendida;
 import com.ceiba.tiendatecnologica.dominio.Producto;
 import com.ceiba.tiendatecnologica.dominio.servicio.garantia.ServicioGenerarGarantia;
 import com.ceiba.tiendatecnologica.dominio.servicio.producto.ServicioObtenerProducto;
+import com.ceiba.tiendatecnologica.dominio.servicio.vendedor.ServicioVendedor;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
 @Component
 public class ManejadorGenerarGarantia {
 
-	private final ServicioObtenerProducto servicioObtenerProducto;
+	/*private final ServicioObtenerProducto servicioObtenerProducto;
 	private final FabricaGarantia fabricaGarantia;
-	private final ServicioGenerarGarantia servicioGenerarGarantia;
+	private final ServicioGenerarGarantia servicioGenerarGarantia;*/
+
+	private final ServicioVendedor servicioVendedor;
 
 
-	public ManejadorGenerarGarantia(ServicioObtenerProducto servicioObtenerProducto, FabricaGarantia fabricaGarantia ,
+	/*public ManejadorGenerarGarantia(ServicioObtenerProducto servicioObtenerProducto, FabricaGarantia fabricaGarantia ,
 									ServicioGenerarGarantia servicioGenerarGarantia) {
 		this.servicioObtenerProducto = servicioObtenerProducto;
 		this.fabricaGarantia = fabricaGarantia;
 		this.servicioGenerarGarantia = servicioGenerarGarantia;
+	}*/
+	public ManejadorGenerarGarantia(ServicioVendedor servicioVendedor) {
+		this.servicioVendedor = servicioVendedor;
 	}
 
-	
 	@Transactional
 	public void ejecutar(String codigoProducto,String nombreCliente) {
-		Producto producto = this.servicioObtenerProducto.ejecutar(codigoProducto);
+		servicioVendedor.generarGarantia(codigoProducto,nombreCliente);
+		/*Producto producto = this.servicioObtenerProducto.ejecutar(codigoProducto);
 		GarantiaExtendida garantiaExtendida = this.fabricaGarantia.crearGarantiaExtendida(producto);
-		this.servicioGenerarGarantia.ejecutar(garantiaExtendida);
+		this.servicioGenerarGarantia.ejecutar(garantiaExtendida);*/
 	}
 }
--- src/main/java/com/ceiba/tiendatecnologica/dominio/servicio/vendedor/ServicioVendedor.java
+++ src/main/java/com/ceiba/tiendatecnologica/dominio/servicio/vendedor/ServicioVendedor.java
@@ -4,11 +4,12 @@ import com.ceiba.tiendatecnologica.dominio.excepcion.GarantiaExtendidaException;
 import com.ceiba.tiendatecnologica.dominio.repositorio.RepositorioGarantiaExtendida;
 import com.ceiba.tiendatecnologica.dominio.repositorio.RepositorioProducto;
 
-import java.util.Calendar;
-
 public class ServicioVendedor {
 
 	public static final String EL_PRODUCTO_TIENE_GARANTIA = "El producto ya cuenta con una garantía extendida";
+	public static final String EL_PRODUCTO_NO_TIENE_GARANTIA = "Este producto no cuenta con garantía extendida";
+	public static final int CANTIDAD_MAXIMA_VOCALES = 3;
+	public static final double PRECIO_MINIMO_PARA_GARANTIA = 500000;
 
 	private RepositorioProducto repositorioProducto;
 	private RepositorioGarantiaExtendida repositorioGarantia;
@@ -18,10 +19,50 @@ public class ServicioVendedor {
 		this.repositorioGarantia = repositorioGarantia;
 	}
 
-	public void generarGarantia(String codigo) {
-		throw new UnsupportedOperationException("Método pendiente por implementar");
+	public void generarGarantia(String codigo, String nombreCliente) {
 
+		if (!tieneGarantia(codigo)){
+			if (codigoValidoParaGenerarGarantia(codigo)){
+				double precio = calcularPrecioGarantia(repositorioProducto.obtenerPorCodigo(codigo).getPrecio());
+				//TODO: implementar codigo para cumplir con requisitos
+				//llamar metodo
+			}else{
+				throw new GarantiaExtendidaException(EL_PRODUCTO_NO_TIENE_GARANTIA);
+			}
+		}else{
+			throw new GarantiaExtendidaException(EL_PRODUCTO_TIENE_GARANTIA);
+		}
 	}
 
-	public boolean tieneGarantia(String codigo) { return false; }
+	public boolean tieneGarantia(String codigo) {
+		return repositorioGarantia.
+				obtenerProductoConGarantiaPorCodigo(codigo)!= null; }
+
+	private static boolean codigoValidoParaGenerarGarantia(String codigo) {
+		return contarVocalesCodigoProducto(codigo) != CANTIDAD_MAXIMA_VOCALES;
+	}
+
+	private static boolean caracterEsVocal(char letra) {
+		return "aeiou".contains(String.valueOf(letra).toLowerCase());
+	}
+
+	private static int contarVocalesCodigoProducto(String codigo) {
+		int cantidadVocales = 0;
+		for (int i= 0; i < codigo.length(); i++){
+			if(caracterEsVocal(codigo.charAt(i))) cantidadVocales ++;
+		}
+		return cantidadVocales;
+	}
+
+	private static double calcularPrecioGarantia(double costoProducto) {
+		double precioGarantia = 0;
+
+		if(costoProducto> PRECIO_MINIMO_PARA_GARANTIA) {
+			precioGarantia = precioGarantia * 0.2;
+			//TODO: aplicar logica de negocio
+
+		}
+		return precioGarantia;
+	}
+
 }
